964523fd782534412e1c2c8506bf168651b5794d
StenidoS <os8333324@gmail.com>
2022-08-05 19:54:38 +0300

    modified

--- src/Engine.php
+++ src/Engine.php
@@ -7,7 +7,7 @@ use function cli\prompt;
 
 function salute()
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $a = prompt('May I have your name?');
     line("Hello, %s!", $a);
     return $a;
