7e85e9f2ce631d51efdb20a0db670d302bc97e6a
StenidoS <os8333324@gmail.com>
2022-08-11 17:43:53 +0300

    modified

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -6,20 +6,6 @@ use function Engine\playGame;
 
 use const Engine\ROUNDS_COUNT;
 
-function calculate(string $operator, int $number1, int $number2): int
-{
-    switch ($operator) {
-        case '+':
-            return $number1 + $number2;
-        case '-':
-            return $number1 - $number2;
-        case '*':
-            return $number1 * $number2;
-        default:
-            throw new \Exception('Error');
-    }
-}
-
 function playCalculate(): void
 {
     $task = 'What is the result of the expression?';
@@ -27,12 +13,11 @@ function playCalculate(): void
     for ($i = 0; $i < ROUNDS_COUNT; $i++) {
         $randomNumber1 = random_int(0, 10);
         $randomNumber2 = random_int(0, 10);
-        $operator = ['*', '+', '-'];
-        $randomOperator = $operator[random_int(0, 2)];
-        $correctAnswer = calculate($randomOperator, $randomNumber1, $randomNumber2);
+        $input = ['*' => $randomNumber1 * $randomNumber2, '+' => $randomNumber1 + $randomNumber2, '-' => $randomNumber1 - $randomNumber2];
+        $randomOperator = array_rand($input, 1);
+        $correctAnswer = $input[$randomOperator];
         $question = "{$randomNumber1 } {$randomOperator } {$randomNumber2}";
         $gameData[] = ['question' => $question, 'correctAnswer' => (string)$correctAnswer];
     }
-
     playGame($task, $gameData);
 }
