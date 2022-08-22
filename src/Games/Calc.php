61b5d8395192ad4abdcabbab1fbbe3685a9de340
StenidoS <os8333324@gmail.com>
2022-08-22 13:16:46 +0300

    modified

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -13,15 +13,25 @@ function playCalculate(): void
     for ($i = 0; $i < ROUNDS_COUNT; $i++) {
         $randomNumber1 = random_int(0, 10);
         $randomNumber2 = random_int(0, 10);
-        $input = [
-            '*' => $randomNumber1 * $randomNumber2,
-            '+' => $randomNumber1 + $randomNumber2,
-            '-' => $randomNumber1 - $randomNumber2
-        ];
-        $randomOperator = array_rand($input);
-        $correctAnswer = $input[$randomOperator];
+        $input = ['+', '-', '*'];
+        $randomOperator = $input[array_rand($input)];
+        $correctAnswer = CalcOperatorRandom($randomNumber1, $randomNumber2, $randomOperator);
         $question = "{$randomNumber1} {$randomOperator} {$randomNumber2}";
         $gameData[] = ['question' => $question, 'correctAnswer' => (string)$correctAnswer];
     }
     playGame(DESCRIPTION, $gameData);
+
+    function CalcOperatorRandom(int $randomNumber1, int $randomNumber2, string $randomOperator)
+    {
+        switch ($randomOperator) {
+            case "+":
+                return $randomNumber1 + $randomNumber2;
+            case "-":
+                return $randomNumber1 - $randomNumber2;
+            case "*":
+                return $randomNumber1 * $randomNumber2;
+            default:
+                return "Incorrect sign: '{$randomOperator}'";
+        }
+    }
 }
